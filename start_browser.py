a2e4a701be6daf6cc9de8e31b06597f89f074ddc
liliang <liang.li01@chaitin.com>
2020-01-23 00:26:40 +0800

    update

--- start_browser.py
+++ start_browser.py
@@ -5,12 +5,29 @@ import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+import random
+import string
+from itertools import product
 
 # 参考：https://www.cnblogs.com/tobecrazy/p/4570494.html
 driver = webdriver.Chrome()
 
 driver.get("https://gitee.com/login")
 time.sleep(5)
+
+# 基础方法
+#获取邮箱方法:namenum(邮箱名字的位数)，mailcount(邮箱的个数)
+def make_email(namenum,mailcount):
+    email = []
+    data = string.ascii_letters+string.digits #字母和数字,特殊字符 string.ascii_lowercase、string.punctuation、string.ascii_uppercase
+    email_type = random.choice(('@163.com', '@qq.com', '@sina.com', '@126.com'))
+    for i in range(mailcount):
+        data = random.sample(data, namenum)
+        email_start = ''.join(data)
+        email.append(email_start+email_type)
+    return email
+
+make_email(5,3)
 
 # 判断元素是否存在(等待加载完成后查找)
 '''
